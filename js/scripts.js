2eb51a717de5a7c1e60020dda3b732d09f7c1a3a
Target52 <gulien@me.com>
2021-10-12 10:01:51 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+let ikRockArrowFunctions2 = () => console.log("Joe, ik rock de arrow functions!");
+
+let fivePlusSeven = () => 5 + 7;
+
+
+let myFunction = (a, b) => a + b;
+
+let addFive = a => a + 5;
+
+let createObject = () => ({ greeting: "hoi" });
